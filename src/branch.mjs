aea63668642c738cdc6ea7bc12378185d0e3ef06
Markus Felten <Markus.Felten@db.com>
2020-09-06 22:06:14 +0200

    wip

---
+++ src/branch.mjs
@@ -0,0 +1,9 @@
+export function CachedBranch(branch) {
+  const entries = new Map();
+
+  return new Proxy(branch,{
+
+    entries: (pattern) {},
+    entry: (name) {}
+  });
+} 
--- src/provider.mjs
+++ src/provider.mjs
@@ -1,23 +1,3 @@
-import { Provider } from "repository-provider";
 
-/**
- * Caches access to a repsository provider
- * @param {Provider} upstreamProvider to be cached
- * @param {Object} options
- * @property {Provider} provider
- */
-export class CacheProvider extends Provider {
-  constructor(upstreamProvider, options) {
-    super(options);
-    Object.defineProperties(this, {
-      upstreamProvider: { value: upstreamProvider }
-    });
-  }
+export * from "./branch.mjs";
 
-  async repository(name) {
-    const proxy3 = new Proxy(upstreamProvider, {});
-
-    const repository = await upstreamProvider.repository(name);
-    return repository;
-  }
-}
