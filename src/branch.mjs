f0f75f518fd54ffc9e9b6709daaf3c9eb729a205
Markus Felten <Markus.Felten@gmx.de>
2022-02-16 12:30:42 +0100

    chore(deps): lock

--- src/branch.mjs
+++ src/branch.mjs
@@ -1,4 +1,10 @@
-export function CachedBranch(branch) {
+
+/**
+ * Generate a cached view of a branch.
+ * @param {Branch} branch 
+ * @returns {Branch} witch cached entries
+ */
+export function cachedBranch(branch) {
   const entries = new Map();
 
   return new Proxy(branch,{
